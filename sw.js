7d51bff2ae7d7a5ea57bd5cc7150d2322cc2142d
taklabassam-pixel <taklabassam@gmail.com>
2025-12-02 02:10:07 +0200

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'sho amli akel?-v2';
 const urlsToCache = [
   '/', // الصفحة الرئيسية (إذا كانت index.html أو /)
   'https://fonts.googleapis.com/css2?family=Amiri:wght@400;700&display=swap',
-  '/splash.html',
+  '/index.html',
   '/index_prompt1.html',
   '/mealsdatabase1.js',
   '/analyse_meals_m.js',
@@ -78,3 +78,4 @@ self.addEventListener('activate', event => {
   return self.clients.claim();
 
 });
+
